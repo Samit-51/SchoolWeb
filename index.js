273fcad07be6255a744f467a6484168cd9480773
Samit <samzenfriend@gmail.com>
2025-02-06 00:04:04 +0545

    Minor changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const logo = document.querySelector(".logo");
 const closeBtn = document.getElementById("close-btn");
+const hamburger = document.getElementById("hamburger");
+const mainLinks = document.querySelector(".main-links");
 const navBtnContainer = document.querySelector(".nav-btns");
 const popup = document.querySelector('.popup');
+
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 20) {
@@ -11,24 +14,35 @@ window.addEventListener("scroll", () => {
     }
 });
 
+
 closeBtn.addEventListener('click', () => {
     popup.style.display = 'none';
+
 });
 
+
 window.addEventListener('click', (e) => {
-    if (!popup.contains(e.target)) {
+    if (!popup.contains(e.target) && !e.target.matches('.popup')) {
         popup.style.display = 'none';
     }
 });
 
+
 navBtnContainer.addEventListener("click", (e) => {
+    if(!e.target.closest('button')) return;
     const targetId = "slide" + e.target.id;
     const activeBtn = navBtnContainer.querySelector(".active");
     const targetBtn = document.getElementById(e.target.id);
-    const targetSlde = document.getElementById(targetId);
-    if (targetSlde) {
-        targetSlde.scrollIntoView({ behavior: "smooth", inline: "start" });
+    const targetSlide = document.getElementById(targetId);
+    if (targetSlide) {
+        targetSlide.scrollIntoView({ behavior: "smooth", inline: "start", block: "nearest"});
     }
     activeBtn.classList.remove("active");
     targetBtn.classList.add("active");
 });
+
+
+hamburger.addEventListener("click", () => {
+    mainLinks.classList.toggle("active"); 
+});
+
