d2fb3151e7b7882135278e0a3c22ce3fc1c2988c
Samit-51 <devilgamingbrawlstars@gmail.com>
2025-03-10 21:47:40 +0545

    Fix path bug(attempt 2)

--- index.js
+++ index.js
@@ -130,3 +130,40 @@ showChairmanBtn.addEventListener('click', function () {
         showChairmanBtn.classList.add('active');
     }
 });
+
+
+fetch("./admin/content.json")
+    .then(response => response.json())
+    .then(data => {
+        const currentPage = document.body.getAttribute("data-page");
+
+        if (data[currentPage]) {
+
+            pageName = data[currentPage];
+            Object.keys(pageName).forEach(sectionClass => {
+                let section = document.querySelector(`.${sectionClass}`);
+                if (section) {
+                    Object.keys(pageName[sectionClass]).forEach(elementClass => {
+                        let element = section.querySelector(`.${elementClass}`);
+                        if (element) {
+                            let contentData = pageName[sectionClass][elementClass];
+                            let dataType = contentData.type;
+                            if (dataType != 'object') {
+                                element[dataType] = contentData.content;
+                            } else {
+                                Object.keys(contentData.content).forEach(innerElementClass => {
+                                    let innerElement = element.querySelector(`.${innerElementClass}`);
+                                    if (innerElement) {
+                                        let innerContent = contentData.content[innerElementClass];
+                                        let dataType = innerContent.type;
+                                        innerElement[dataType] = innerContent.content;
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    })
+    .catch(error => console.error("Error loading content:", error));
